c3b079858b29381656020963ab90737d3141c7f8
Sarthak Pande <107572589+sarthakpande108@users.noreply.github.com>
2025-02-01 17:57:05 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    pdf_content=input_pdf_setup(upload_file)
-            response=get_Gemini_response(input_Prompt2,pdf_content,input_text)
-            st.subheader("the responses is")
-            st.write(response)
